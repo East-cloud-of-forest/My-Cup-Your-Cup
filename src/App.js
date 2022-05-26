1cb61e913b1f44a5a03d5cb8c076b5dcb272e636
dongun lim <ecof1911@gmail.com>
2022-05-26 21:32:29 +0900

    메인페이지 - 헤더부분 추가, 홈 컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -4,7 +4,29 @@ import HomeComp from './components/HomeComp'
 function App() {
   return (
     <div className="App">
-      <HomeComp />
+      <header id="App_Header">
+        <div>
+          <ul id="Top_nav" className="caption">
+            <li>검색</li>
+            <li>로그인</li>
+            <li>회원가입</li>
+          </ul>
+        </div>
+        <h1>로고</h1>
+        <nav>
+          <ul>
+            <li>텀블러 제작</li>
+            <li>모두의 디자인</li>
+            <li>리뷰</li>
+            <li>문의하기</li>
+          </ul>
+        </nav>
+        <hr></hr>
+      </header>
+
+      <main>
+        <HomeComp />
+      </main>
     </div>
   );
 }
