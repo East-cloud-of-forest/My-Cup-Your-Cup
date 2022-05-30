0745b4a4a59a93576b1d7dbd5d3643358af4e911
Jinyooo <kjyong93@ewhain.net>
2022-05-30 15:41:00 +0900

    마이페이지 - profile.scss 추가, 나머지 컴포넌트 추가

---
+++ src/components/Mypage/AllDesigns.jsx
@@ -0,0 +1,18 @@
+export default function AllDesigns() {
+    return (
+        <>
+            <h4>전체보기</h4>
+            <a href="#">더보기</a>
+            <ul className="design_thumb">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </>
+    )
+}
--- src/components/Mypage/MypageComp.jsx
+++ src/components/Mypage/MypageComp.jsx
@@ -1,6 +1,17 @@
+import Profile from "./Profile";
+import AllDesigns from "./AllDesigns";
+import MyDesigns from "./MyDesigns";
+import LikedDesigns from "./LikedDesigns"
+
 const MypageComp = () => {
     return (
-        <>마이페이지</>
+        <>
+            <h1>마이페이지</h1>
+            <Profile />
+            <AllDesigns />
+            <MyDesigns />
+            <LikedDesigns />
+        </>
     )
 }
 export default MypageComp;
