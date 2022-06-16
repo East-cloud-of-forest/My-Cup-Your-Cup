6a2c1fdfc8f3bb0a4382d554cbabeedbc07e197f
ecof <ecof1911@gmail.com>
2022-06-16 17:51:56 +0900

    회원가입 동의서 페이지 아코디언 완성 및 동의서 수정
    react 에서 html 사용시 dangerouslySetInnerHTML 을 사용
    lable 을 이용해 checkbox 커스텀
    flex 내에서 aspect ratio 사용시 자리가 부족하면 줄어들어 height 값으로
    재조정

--- src/pages/Agreement/Agreement.jsx
+++ src/pages/Agreement/Agreement.jsx
@@ -1,25 +1,30 @@
+import classNames from 'classnames'
 import { useEffect } from 'react'
 import { useState } from 'react'
 import { Accordion } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import agreeform from '../../components/AgreementComp/personal'
+import './Agreement.scss'
 
 const Agreement = () => {
   const [accordion, setAccordion] = useState([
     {
+      ...agreeform[0],
       key: '0',
       active: false,
+      required: true,
     },
     {
+      ...agreeform[1],
       key: '1',
       active: false,
+      required: true,
     },
     {
+      ...agreeform[2],
       key: '2',
       active: false,
-    },
-    {
-      key: '3',
-      active: false,
+      required: false,
     },
   ])
   const activeKey = accordion.map((a) => !a.active && a.key)
@@ -34,6 +39,7 @@ const Agreement = () => {
       }),
     )
   }
+
   const allCange = (e) => {
     setAccordion(
       accordion.map((accordion) => {
@@ -42,6 +48,16 @@ const Agreement = () => {
       }),
     )
   }
+
+  const allClick = (e) => {
+    setAccordion(
+      accordion.map((accordion) => {
+        accordion.active = !e.currentTarget.children[0].checked
+        return accordion
+      }),
+    )
+  }
+
   useEffect(() => {
     accordion.filter((a) => a.active == true).length == accordion.length
       ? setAllActive(true)
@@ -49,25 +65,45 @@ const Agreement = () => {
   }, [accordion])
 
   return (
-    <div>
-      <input type="checkbox" checked={allActive} onChange={allCange} />
-      <span>전체동의</span>
+    <div className="agreement_page">
+      <div className='allagree_block' onClick={allClick}>
+        <input
+          type="checkbox"
+          checked={allActive}
+          onChange={allCange}
+          id="agreecheckall"
+        />
+        <label htmlFor="agreecheckall" className='agreecheckall'></label>
+        <span>내잔네잔 서비스 이용약관 및 개인정보 처리방침, 전자상거래 표준약관, 개인정보 제 3자 제공에 관한 사항(선택)에 모두 동의합니다.</span>
+      </div>
       <Accordion alwaysOpen activeKey={activeKey}>
         {accordion.map((a, i) => (
           <Accordion.Item eventKey={a.key} key={i}>
-            <Accordion.Header onClick={() => onClick(a.key)}>
-              <input type="checkbox" checked={a.active} readOnly />
-              Accordion Item #1
+            <Accordion.Header
+              onClick={() => onClick(a.key)}
+              className={classNames(a.required ? null : 'noneRequired')}
+            >
+              <p>{a.title}</p>
+              <div>
+                <span className="caption">
+                  ({a.required ? '필수' : '선택'})
+                </span>
+                <input
+                  type="checkbox"
+                  checked={a.active}
+                  readOnly
+                  id={'agreecheck' + i}
+                />
+                <label
+                  htmlFor={'agreecheck' + i}
+                  onClick={() => onClick(a.key)}
+                ></label>
+              </div>
             </Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Accordion.Body>
+            <Accordion.Body
+              className="accodion_body"
+              dangerouslySetInnerHTML={{ __html: a.content }}
+            />
           </Accordion.Item>
         ))}
       </Accordion>
