49f7fa3640ac8fa0c0dc412d0167c5231036766b
dongun lim <ecof1911@gmail.com>
2022-06-27 20:18:35 +0900

    충돌 오류 수정

--- src/components/index-comp/IndexComp.jsx
+++ src/components/index-comp/IndexComp.jsx
@@ -51,13 +51,8 @@ export const ButtonComp = (props) => {
         color,
       )}
       onClick={(e) => {
-<<<<<<< HEAD
         clickanimation(e);
         onClick(e);
-=======
-        clickanimation(e)
-        onClick()
->>>>>>> fbaba38b2358577eb10a69696506d8cc12ad18c5
       }}
     >
       <span>{children}</span>
