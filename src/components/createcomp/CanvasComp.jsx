4e78ca9d41e561f29f4dcc61836f28b49c72afff
dongun lim <ecof1911@gmail.com>
2022-07-03 23:34:08 +0900

    선택 가능할시 커서 변경, 모서리 원 추가, 모서리 원 커서 변경

--- src/components/createcomp/CanvasComp.jsx
+++ src/components/createcomp/CanvasComp.jsx
@@ -17,6 +17,7 @@ const CanvasComp = ({ pic, texts, colorData, setTexts }) => {
       this.mouseDown = false;
 
       this.Text = [];
+      this.circle = [];
       this.onMouseText = null;
       this.selectOnText = selectOnText;
 
@@ -34,6 +35,8 @@ const CanvasComp = ({ pic, texts, colorData, setTexts }) => {
       this.canvas.addEventListener("pointermove", this.onMoveHandler, false);
       this.onUpHandler = this.onUp.bind(this);
       this.canvas.addEventListener("pointerup", this.onUpHandler, false);
+      this.mouseOverHandler = this.mouseOver.bind(this);
+      this.canvas.addEventListener("mousemove", this.mouseOverHandler, false);
     }
 
     resize() {
@@ -43,17 +46,20 @@ const CanvasComp = ({ pic, texts, colorData, setTexts }) => {
       this.canvas.width = this.stageWidth;
       this.canvas.height = this.stageHeight;
 
+      for (let i = 0; i < 4; i++) {
+        this.circle.push(new Circle());
+      }
       texts.map((text, i) => {
         this.Text[i] = new Text(
           text.text,
           text.font,
           text.color,
-          text.height,
           i,
           text.x,
           text.y,
           this.stageWidth,
-          this.stageHeight
+          this.stageHeight,
+          this.circle
         );
       });
     }
@@ -79,7 +85,7 @@ const CanvasComp = ({ pic, texts, colorData, setTexts }) => {
       );
       this.mouseDown = true;
       const selectdArray = this.Text.map((text) => {
-        const selectid = text.mouseDown(this.mousePos);
+        const selectid = text.onDown(this.mousePos);
         if (selectid !== undefined) {
           this.onMouseText = selectid;
           this.selectOnText = selectid;
@@ -100,15 +106,44 @@ const CanvasComp = ({ pic, texts, colorData, setTexts }) => {
         e.clientY - e.currentTarget.getBoundingClientRect().top
       );
       this.Text.forEach((text) => {
-        text.move(this.mousePos, this.onMouseText);
+        text.onMove(this.mousePos, this.onMouseText);
       });
     }
 
     onUp(e) {
       this.mouseDown = false;
       this.Text.forEach((text) => {
-        text.mouseUp();
+        text.onUp();
       });
+    }
+
+    // 마우스 커서 변경
+    mouseOver(e) {
+      this.mousePos.x = parseInt(
+        e.clientX - e.currentTarget.getBoundingClientRect().left
+      );
+      this.mousePos.y = parseInt(
+        e.clientY - e.currentTarget.getBoundingClientRect().top
+      );
+      const boxArray = this.Text.map((text) =>
+        text.mouseOver(this.mousePos)
+      );
+      const circleArray = this.circle.map((c) => c.mouseOver(this.mousePos));
+      if (circleArray.filter((a) => a === true).length !== 0) {
+        switch (circleArray.indexOf(true)) {
+          case 0:
+          case 2:
+            this.canvas.style.cursor = "nw-resize";
+            break;
+          default:
+            this.canvas.style.cursor = "ne-resize";
+            break;
+        }
+      } else if (boxArray.filter((a) => a === true).length !== 0) {
+        this.canvas.style.cursor = "Move";
+      } else {
+        this.canvas.style.cursor = "Default";
+      }
     }
 
     remove() {
@@ -118,25 +153,37 @@ const CanvasComp = ({ pic, texts, colorData, setTexts }) => {
       this.canvas.removeEventListener("pointerdown", this.onDownHandler);
       this.canvas.removeEventListener("pointermove", this.onMoveHandler);
       this.canvas.removeEventListener("pointerup", this.onUpHandler);
+      this.canvas.removeEventListener("mousemove", this.mouseOverHandler);
     }
   }
 
+  // text
   class Text {
-    constructor(text, font, color, height, id, x, y, stageWidth, stageHeight) {
+    constructor(text, font, color, id, x, y, stageWidth, stageHeight, circle) {
       this.text = text;
       this.font = font;
       this.color = color;
       this.x = x === -1000 ? stageWidth / 2 : x;
       this.y = y === -1000 ? stageHeight / 2 : y;
       this.id = id;
+      this.circle = circle;
     }
 
-    mouseDown(pos) {
+    mouseOver(pos) {
       this.select = // 왼 오 위 아래
         pos.x >= this.x - this.width / 2 &&
-        pos.x <= this.x + this.width / 2 + 2 &&
-        pos.y >= this.y + this.height * 0.1 - this.height &&
-        pos.y <= this.y + this.height * 0.1;
+        pos.x <= this.x + this.width / 2 + 1 &&
+        pos.y >= this.y - this.height + 10 &&
+        pos.y <= this.y + 10;
+      return this.select;
+    }
+
+    onDown(pos) {
+      this.select = // 왼 오 위 아래
+        pos.x >= this.x - this.width / 2 &&
+        pos.x <= this.x + this.width / 2 + 1 &&
+        pos.y >= this.y - this.height + 10 &&
+        pos.y <= this.y + 10;
       if (this.select) {
         this.clickposX = pos.x;
         this.clickposY = pos.y;
@@ -144,7 +191,7 @@ const CanvasComp = ({ pic, texts, colorData, setTexts }) => {
       }
     }
 
-    move(pos, id) {
+    onMove(pos, id) {
       if (this.select && this.id === id) {
         const dx = pos.x - this.clickposX;
         const dy = pos.y - this.clickposY;
@@ -157,7 +204,7 @@ const CanvasComp = ({ pic, texts, colorData, setTexts }) => {
       }
     }
 
-    mouseUp() {
+    onUp() {
       if (this.select) {
         const textsclone = [...texts];
         textsclone[this.id] = {
@@ -176,21 +223,66 @@ const CanvasComp = ({ pic, texts, colorData, setTexts }) => {
       ctx.textAlign = "center";
       ctx.fillStyle = this.color;
       const text = ctx.measureText(this.text);
-      this.width = Number(text.width.toFixed(0)) * 1.1;
+      this.width = Number(text.width.toFixed(0)) + 20;
       this.height =
-        (text.actualBoundingBoxAscent + text.actualBoundingBoxDescent) * 1.2;
+        text.actualBoundingBoxAscent +
+        (text.actualBoundingBoxDescent < 0
+          ? 0
+          : text.actualBoundingBoxDescent) +
+        20;
       ctx.fillText(this.text, this.x, this.y);
 
       // 선택 박스
       if (id === this.id) {
-        ctx.strokeStyle = "green";
+        // 박스 선
+        ctx.beginPath();
+        ctx.strokeStyle = "#4debbb";
+        ctx.lineWidth = 1;
         ctx.strokeRect(
           this.x - this.width / 2,
-          this.y - this.height + this.height * 0.1,
+          this.y - this.height + 10,
           this.width,
           this.height
         );
+        // 모서리 원
+        this.circle[0].draw(
+          ctx,
+          this.x - this.width / 2,
+          this.y - this.height + 10
+        );
+        this.circle[1].draw(
+          ctx,
+          this.x + this.width / 2,
+          this.y - this.height + 10
+        );
+        this.circle[2].draw(ctx, this.x + this.width / 2, this.y + 10);
+        this.circle[3].draw(ctx, this.x - this.width / 2, this.y + 10);
       }
+    }
+  }
+
+  class Circle {
+    constructor() {
+      this.PI = Math.PI * 2;
+      this.radius = 4;
+    }
+
+    mouseOver(pos) {
+      const dx = pos.x - this.x;
+      const dy = pos.y - this.y;
+      this.mouseon = dx * dx + dy * dy <= this.radius * this.radius;
+      return this.mouseon;
+    }
+
+    draw(ctx, x, y) {
+      this.x = x;
+      this.y = y;
+
+      ctx.beginPath();
+      ctx.fillStyle = "#3da982";
+      ctx.arc(x, y, this.radius, 0, this.PI, false);
+      ctx.closePath();
+      ctx.fill();
     }
   }
 
