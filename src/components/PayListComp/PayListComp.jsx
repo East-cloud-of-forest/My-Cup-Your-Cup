5f7daab434aef8d1b1585fb2aa6e28661297dbf9
DenierDenier <cn930818@gmail.com>
2022-07-05 17:52:42 +0900

    'paylist_Page_보완'

--- src/components/PayListComp/PayListComp.jsx
+++ src/components/PayListComp/PayListComp.jsx
@@ -9,6 +9,8 @@ const PayListComp = () => {
   const { user } = useSelector((a) => a.enteruser)
   
   const [list, setList] = useState([]);
+  const [dateList, setDateList] = useState([]);
+  const [fList,setFList] = useState([]);
 
   const buyList =  async () => {
     
@@ -19,13 +21,37 @@ const PayListComp = () => {
 
     //배열로 데이터의 키를 배열로 변환하여 아이템리스트의 인덱스를 구함
     const listIndex = Object.keys(userData).indexOf("itemsList")
-
+    
     //아이템리스트의 값을 반환하게 함
     const itemListValue = Object.values(userData)[listIndex];
-
+    
     console.log(itemListValue)
     
     setList(itemListValue)
+
+    console.log(list);
+    
+    //날짜 모으기
+    const dateArr = []
+
+    for (let i = 0; i < list.length; i++) {
+      dateArr.push(list[i].boughtDate);
+    }
+    //중복 제거된 날짜 배열
+    const dateNewArr = [...new Set(dateArr)]
+    setDateList(dateNewArr)
+
+    const finalList =[];
+
+    for (let i = 0; i < dateList.length; i++) {
+      finalList.push(
+        { [dateList[i]] : list.filter( l => l.boughtDate == dateList[i])}
+      )
+    }
+
+    setFList(finalList)
+    
+    console.log(fList[0])
   }
 
   useEffect(()=>{
@@ -36,13 +62,20 @@ const PayListComp = () => {
     <div>
       <hr className="pay_main_hr"/>
 
-        {list.map((a,i) => (
-          <div key={i}>
-            <img src={a.itemImage} alt="" />
-            <p>{a.itemName} - {a.itemSize}</p>
+      {fList.map((d,i)=>(
+        <div>
+          <h3 key={i}></h3>
+        </div>
+      ))}
+
+        {/* {list.map((t,index) => (
+          
+          <div key={index}>
+            <img src={t.itemImage} alt="" />
+            <p>{t.itemName} - {t.itemSize}</p>
             <hr />
           </div>
-        ))}
+        ))} */}
 
       <hr className="pay_main_hr"/>
     </div>
