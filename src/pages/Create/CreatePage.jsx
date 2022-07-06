58639e268bb83f8097e8d40b969d6278dde26c3f
dongun lim <ecof1911@gmail.com>
2022-07-06 23:05:11 +0900

    canvas side edit 위치 변경, layer 추가, top edit 삭제

--- src/pages/Create/CreatePage.jsx
+++ src/pages/Create/CreatePage.jsx
@@ -1,150 +1,150 @@
-import './Create.scss'
-import SelectComp from '../../components/createcomp/SelectComp'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import "./Create.scss";
+import SelectComp from "../../components/createcomp/SelectComp";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faWhiskeyGlass,
   faFileArrowUp,
   faFont,
   faStar,
-} from '@fortawesome/free-solid-svg-icons'
-import { ButtonComp } from '../../components/index-comp/IndexComp'
-import React, { useState, useEffect } from 'react'
-import classNames from 'classnames'
-import CanvasComp from '../../components/createcomp/CanvasComp'
-import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
-import CanvasSelectComp from '../../components/createcomp/CanvasSelectComp'
+} from "@fortawesome/free-solid-svg-icons";
+import { ButtonComp } from "../../components/index-comp/IndexComp";
+import React, { useState, useEffect } from "react";
+import classNames from "classnames";
+import CanvasComp from "../../components/createcomp/CanvasComp";
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
+import CanvasSelectComp from "../../components/createcomp/CanvasSelectComp";
 
 const CreatePage = () => {
   //재질 props 받아오는 함수
-  const [material, setMaterial] = useState('')
+  const [material, setMaterial] = useState("");
 
   const getTypeData = (material) => {
-    setMaterial(material)
-  }
+    setMaterial(material);
+  };
 
   //컬러 props
-  const [colorData, setColorData] = useState('#FFFFFF')
+  const [colorData, setColorData] = useState("#FFFFFF");
   const getColorData = (colorData) => {
-    setColorData(colorData)
-  }
+    setColorData(colorData);
+  };
 
   //상품명과 색상명 props 받아오는 함수
-  const [productName, setProductName] = useState('상품명')
+  const [productName, setProductName] = useState("상품명");
   const getProductName = (productName) => {
-    setProductName(productName)
-  }
+    setProductName(productName);
+  };
 
-  const [colorName, setColorName] = useState('흰색')
+  const [colorName, setColorName] = useState("흰색");
   const getColorName = (colorName) => {
-    setColorName(colorName)
-  }
+    setColorName(colorName);
+  };
 
   //아코디언 버튼
-  const [active, setActive] = useState(false)
-  const [display, setDisplay] = useState(true)
+  const [active, setActive] = useState(false);
+  const [display, setDisplay] = useState(true);
   const timeToggle = function (kind, time) {
     setTimeout(() => {
-      kind === 'active' ? setActive(!active) : setDisplay(!display)
-    }, time)
-  }
+      kind === "active" ? setActive(!active) : setDisplay(!display);
+    }, time);
+  };
 
   const openClick = () => {
-    console.log(1)
-    clearTimeout(timeToggle)
+    console.log(1);
+    clearTimeout(timeToggle);
     if (display) {
-      setDisplay(!display)
-      timeToggle('active', 0)
+      setDisplay(!display);
+      timeToggle("active", 0);
     } else {
-      setActive(!active)
-      timeToggle('display', 500)
+      setActive(!active);
+      timeToggle("display", 500);
     }
-  }
+  };
 
   //텀블러 변경 및 텀블러 이름값 가져오기
-  const [tumShape, setTumShape] = useState('')
+  const [tumShape, setTumShape] = useState("");
   const changeTum = (e) => {
-    setPic(e.currentTarget.id)
-    switch (e.currentTarget.id.split('_')[1]) {
-      case '1':
-        setTumShape('기본형')
-        break
-      case '2':
-        setTumShape('원통형')
-        break
-      case '3':
-        setTumShape('컵형')
-        break
+    setPic(e.currentTarget.id);
+    switch (e.currentTarget.id.split("_")[1]) {
+      case "1":
+        setTumShape("기본형");
+        break;
+      case "2":
+        setTumShape("원통형");
+        break;
+      case "3":
+        setTumShape("컵형");
+        break;
       default:
-        setTumShape('')
-        break
+        setTumShape("");
+        break;
     }
-  }
+  };
 
   // 사이드 에딧트
-  const [sideEditOpen, setSideEditOpen] = useState(false)
+  const [sideEditOpen, setSideEditOpen] = useState(false);
   const sideEditToggle = () => {
-    setSideEditOpen(!sideEditOpen)
-  }
+    setSideEditOpen(!sideEditOpen);
+  };
 
   //재질변경에 따른 내용 변경
   //아코디언 내용 변경
-  const [tumType, setTumType] = useState([])
-  const [pic, setPic] = useState('1')
+  const [tumType, setTumType] = useState([]);
+  const [pic, setPic] = useState("1");
   useEffect(() => {
-    if (material === 'pla') {
+    if (material === "pla") {
       setTumType([
-        { name: '기본형', id: 'pla_1' },
-        { name: '원통형', id: 'pla_2' },
-      ])
-      setPic('pla_1')
-      setTumShape('기본형')
-    } else if (material === 'stain') {
+        { name: "기본형", id: "pla_1" },
+        { name: "원통형", id: "pla_2" },
+      ]);
+      setPic("pla_1");
+      setTumShape("기본형");
+    } else if (material === "stain") {
       setTumType([
-        { name: '기본형', id: 'stain_1' },
-        { name: '원통형', id: 'stain_2' },
-        { name: '컵형', id: 'stain_3' },
-      ])
-      setPic('stain_1')
-      setTumShape('기본형')
+        { name: "기본형", id: "stain_1" },
+        { name: "원통형", id: "stain_2" },
+        { name: "컵형", id: "stain_3" },
+      ]);
+      setPic("stain_1");
+      setTumShape("기본형");
     } else {
-      setTumType([])
-      setPic('1')
-      setTumShape('')
+      setTumType([]);
+      setPic("1");
+      setTumShape("");
     }
-  }, [material])
+  }, [material]);
 
   // 요소 추가
-  const [canvasObjects, setCanvasObjects] = useState([])
-  const [objId, setObjId] = useState(0)
+  const [canvasObjects, setCanvasObjects] = useState([]);
+  const [objId, setObjId] = useState(0);
 
   // 요소 선택
-  const [selectOnObject, setSelectOnObject] = useState(null)
+  const [selectOnObject, setSelectOnObject] = useState(null);
 
   const addText = () => {
-    const objClone = [...canvasObjects]
+    const objClone = [...canvasObjects];
     objClone.push({
-      type: 'text',
-      text: '텍스트',
-      font: 'ROKAFSansBold',
+      type: "text",
+      text: "텍스트",
+      font: "ROKAFSansBold",
       size: 32,
-      color: '#000000',
+      color: "#000000",
       id: objId,
       x: -1000,
       y: -1000,
-    })
-    setCanvasObjects(objClone)
-    setSelectOnObject(canvasObjects.length)
-    sideEditToggle()
-    setObjId(objId + 1)
-  }
+    });
+    setCanvasObjects(objClone);
+    setSelectOnObject(canvasObjects.length);
+    sideEditToggle();
+    setObjId(objId + 1);
+  };
 
   // 파일 추가
   const fileUpload = (e) => {
-    const img = new Image()
+    const img = new Image();
     Array.from(e.target.files).forEach((file) => {
-      img.src = URL.createObjectURL(file)
-    })
-    const imagesclone = [...canvasObjects]
+      img.src = URL.createObjectURL(file);
+    });
+    const imagesclone = [...canvasObjects];
     img.onload = () => {
       imagesclone.push({
         img: img,
@@ -153,56 +153,98 @@ const CreatePage = () => {
         y: -1000,
         width: img.width,
         height: img.height,
-      })
-      setCanvasObjects(imagesclone)
-      setSelectOnObject(canvasObjects.length)
-      sideEditToggle()
-      setObjId(objId + 1)
-    }
-  }
+      });
+      setCanvasObjects(imagesclone);
+      setSelectOnObject(canvasObjects.length);
+      sideEditToggle();
+      setObjId(objId + 1);
+    };
+  };
 
   // 삭제
-  const deletText = () => {
+  const deletText = (i) => {
     const objClone = canvasObjects.filter(
-      (text) => text.id !== canvasObjects[selectOnObject].id,
-    )
-    setCanvasObjects(objClone)
-    setSelectOnObject(null)
-  }
+      (text) => text.id !== canvasObjects[i].id
+    );
+    setCanvasObjects(objClone);
+    setSelectOnObject(null);
+  };
 
   // 전부 삭제
   const allDeleteText = () => {
-    setCanvasObjects([])
-    setSelectOnObject(null)
-    setObjId(0)
-  }
+    setCanvasObjects([]);
+    setSelectOnObject(null);
+    setObjId(0);
+  };
 
+  // 레이어 선택
+  const selectLayer = (i) => {
+    console.log("s");
+    setSelectOnObject(i);
+  };
+
+  // 선택된 요소의 타입
   const selectType = canvasObjects[selectOnObject]
     ? canvasObjects[selectOnObject]
-    : { type: null }
+    : { type: null };
 
   return (
     <div className="cre_all">
       {/**제작화면 */}
       <div className="cre_result">
-        {/**에딧 아이콘 */}
-        <div
-          className={classNames(
-            'cre_edit',
-            selectOnObject !== null ? 'cre_edit_active' : null,
-          )}
-        >
-          <div className="cre_editdiv" onClick={allDeleteText}>
-            <ButtonComp icon>
-              <FontAwesomeIcon icon={solid('note-sticky')} />
+        {/* 레이어 */}
+        <div className={classNames("layer", tumType.length !== 0 && "show")}>
+          <div className="header">
+            레이어
+            <ButtonComp icon onClick={allDeleteText}>
+              <FontAwesomeIcon icon={solid("eraser")} />
             </ButtonComp>
-            전부 삭제
           </div>
-          <div className="cre_editdiv" onClick={deletText}>
-            <ButtonComp icon>
-              <FontAwesomeIcon icon={solid('trash-can')} />
-            </ButtonComp>
-            삭제
+          <div className="layer_body">
+            {canvasObjects.map((item, i) => (
+              <div
+                className={classNames(
+                  "item",
+                  selectOnObject === i ? "select" : null
+                )}
+                key={i}
+                onClick={() => {
+                  selectLayer(i);
+                }}
+              >
+                <div className="icon">
+                  {item.type ? (
+                    <FontAwesomeIcon icon={solid("font")} />
+                  ) : (
+                    <FontAwesomeIcon icon={solid("image")} />
+                  )}
+                </div>
+
+                <div className="content">
+                  {item.type ? (
+                    <p>{item.text}</p>
+                  ) : (
+                    <div className="image_box">
+                      <img src={item.img.currentSrc} alt="item_image" />
+                    </div>
+                  )}
+                </div>
+
+                <div className="btns">
+                  <ButtonComp
+                    icon
+                    onClick={() => {
+                      deletText(i);
+                    }}
+                  >
+                    <FontAwesomeIcon icon={solid("trash-can")} />
+                  </ButtonComp>
+                  <ButtonComp icon>
+                    <FontAwesomeIcon icon={solid("eye")} />
+                  </ButtonComp>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
 
@@ -221,22 +263,22 @@ const CreatePage = () => {
         {/* 사이드 에딧 */}
         <div
           className={classNames(
-            'side_edit',
-            sideEditOpen ? 'side_edit_active' : null,
+            "side_edit",
+            sideEditOpen ? "side_edit_active" : null
           )}
         >
           <div className="side_edit_toggle">
             <ButtonComp
               color="white"
               onClick={() => {
-                sideEditToggle()
-                active && openClick()
+                sideEditToggle();
+                active && openClick();
               }}
             >
-              {sideEditOpen ? (
-                <FontAwesomeIcon icon={solid('caret-right')} />
+              {!sideEditOpen ? (
+                <FontAwesomeIcon icon={solid("caret-right")} />
               ) : (
-                <FontAwesomeIcon icon={solid('caret-left')} />
+                <FontAwesomeIcon icon={solid("caret-left")} />
               )}
             </ButtonComp>
           </div>
@@ -244,32 +286,32 @@ const CreatePage = () => {
             <li>
               <div
                 className={classNames(
-                  'cre_acc_click',
-                  tumType.length === 0 ? 'unactive' : null,
+                  "cre_acc_click",
+                  tumType.length === 0 ? "unactive" : null
                 )}
               >
                 <FontAwesomeIcon
                   icon={faWhiskeyGlass}
                   className="cre_icon2"
                   onClick={() => {
-                    tumType.length !== 0 && openClick()
+                    tumType.length !== 0 && openClick();
                   }}
                 />
                 텀블러변경
               </div>
               <ul
                 className={classNames(
-                  'tumtype_select',
-                  active ? 'active' : null,
-                  display ? 'displaynone' : null,
+                  "tumtype_select",
+                  active ? "active" : null,
+                  display ? "displaynone" : null
                 )}
               >
                 {tumType.map((a, i) => (
                   <li key={i}>
                     <div id={a.id} onClick={changeTum}>
-                      {i === 0 ? <FontAwesomeIcon icon={solid('1')} /> : null}
-                      {i === 1 ? <FontAwesomeIcon icon={solid('2')} /> : null}
-                      {i === 2 ? <FontAwesomeIcon icon={solid('3')} /> : null}
+                      {i === 0 ? <FontAwesomeIcon icon={solid("1")} /> : null}
+                      {i === 1 ? <FontAwesomeIcon icon={solid("2")} /> : null}
+                      {i === 2 ? <FontAwesomeIcon icon={solid("3")} /> : null}
                       {a.name}
                     </div>
                   </li>
@@ -277,32 +319,55 @@ const CreatePage = () => {
               </ul>
             </li>
             <li>
-              <label htmlFor="imageUpload">
-                <div className="cre_editdiv2">
+              {tumType.length === 0 ? (
+                <div className="cre_acc_click unactive">
                   <FontAwesomeIcon icon={faFileArrowUp} className="cre_icon2" />
                   이미지 업로드
                 </div>
-              </label>
-              <input
-                type="file"
-                id="imageUpload"
-                onChange={fileUpload}
-                accept="image/*"
-              />
+              ) : (
+                <>
+                  <label htmlFor="imageUpload">
+                    <div className="cre_acc_click">
+                      <FontAwesomeIcon
+                        icon={faFileArrowUp}
+                        className="cre_icon2"
+                      />
+                      이미지 업로드
+                    </div>
+                  </label>
+                  <input
+                    type="file"
+                    id="imageUpload"
+                    onChange={fileUpload}
+                    accept="image/*"
+                  />
+                </>
+              )}
             </li>
             <li>
-              <div className="cre_editdiv2">
+              <div
+                className={classNames(
+                  "cre_acc_click",
+                  tumType.length === 0 ? "unactive" : null
+                )}
+              >
                 <FontAwesomeIcon
                   icon={faFont}
                   className="cre_icon2"
-                  // onClick={textClick}
-                  onClick={addText}
+                  onClick={() => {
+                    tumType.length !== 0 && addText();
+                  }}
                 />
                 텍스트 추가
               </div>
             </li>
             <li>
-              <div className="cre_editdiv2">
+              <div
+                className={classNames(
+                  "cre_acc_click",
+                  tumType.length === 0 ? "unactive" : null
+                )}
+              >
                 <FontAwesomeIcon icon={faStar} className="cre_icon2" />
                 무료 디자인
               </div>
@@ -312,11 +377,7 @@ const CreatePage = () => {
       </div>
 
       <div className="cre_option_hidden">
-        <div
-          className={
-            !selectType.type ? 'option_active' : 'select_active'
-          }
-        >
+        <div className={!selectType.type ? "option_active" : "select_active"}>
           <SelectComp
             getColorName={getColorName}
             getColorData={getColorData}
@@ -327,6 +388,7 @@ const CreatePage = () => {
             material={material}
             getTypeData={getTypeData}
             productName={productName}
+            setSideEditOpen={setSideEditOpen}
           />
           <CanvasSelectComp
             selectOnObject={selectOnObject}
@@ -337,7 +399,7 @@ const CreatePage = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CreatePage
+export default CreatePage;
