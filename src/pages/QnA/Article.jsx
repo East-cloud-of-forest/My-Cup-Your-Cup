764dbb31d4650d5c501ba21e22f9a10231ec5067
hoyeon <wjdghdus777@naver.com>
2022-07-11 04:17:06 +0900

    게시판 기능 구현중

---
+++ src/pages/QnA/Article.jsx
@@ -0,0 +1,22 @@
+
+
+
+// 여긴 게시글 보는 곳임
+
+
+const Article = (props) => {
+    
+
+    return (
+        <div>
+
+            <h2>{props.title}</h2>
+
+            <p>{props.body}</p>)npmjKC
+
+        </div>
+    )
+}
+
+export default Article
+
--- src/pages/QnA/Ask.jsx
+++ src/pages/QnA/Ask.jsx
@@ -5,30 +5,65 @@ import '../QnA/Ask.scss'
 
 import QnAmenu from './QnAmenu'
 
+import { useState } from 'react'
 
-const Ask = () => {
+
+
+const Ask = (props) => {
+
+    const [category, setCategory] = useState("");
+
+    const categoryChoice = (e) => {
+        setCategory(e.target.value)
+    }
+
+    const date = new Date();
+
+    const year = date.getFullYear()
+    const month = date.getMonth()+1
+    const day = date.getDate()
+
 
     return (
         <div>
             
         <QnAmenu />
 
+        <button onClick={() => {
+
+            let num = 1;
+
+            props.test(num)
+            alert("Ask에서 app.js로 데이터1 전송함")
+
+        }}>데이터 전송 테스트</button>
+
 <main className="Ask_page">
 
 <div className="ask_box">
 
-<form>
+
+<form onSubmit={(e) => {
+
+    let title = e.target.title.value
+    let body = e.target.body.value
+
+    
+    // 게시글 데이터 전송
+    props.newPost(title, body, category, year, month, day)
+}}>
     <hr />
         <div className="title">
             <h3>제목</h3>
-            <input type="text" />
+            <input name="title" type="text" />
         </div>
 
     <hr />
 
         <div className="category">
             <h3>상담 분류</h3>
-            <select className="category_select">
+            <select onChange={categoryChoice} className="category_select">
+                <option value="">카테고리 선택</option>
                 <option value="회원">회원</option>
                 <option value="배송">배송</option>
                 <option value="주문">주문</option>
@@ -40,11 +75,11 @@ const Ask = () => {
 
         <div className="ask_contents">
             <h3>문의내용</h3>
-            <textarea></textarea>
+            <textarea name="body" />
         </div>
     <hr />
 
-    <div className="send_button">문의하기</div>
+    <input type="submit" className="send_button" value="문의하기" />
 </form>
     
 </div>
