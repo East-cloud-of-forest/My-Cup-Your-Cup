75ad08e4c7b3cd1de953a93ecb5f990c060c3341
DenierDenier <cn930818@gmail.com>
2022-07-11 17:10:30 +0900

    paylist 버그 및 일부 수정

--- src/pages/ReviewWirete/ReviewWriteForm.jsx
+++ src/pages/ReviewWirete/ReviewWriteForm.jsx
@@ -229,6 +229,7 @@ const ReviewWriteForm = () => {
   }
 
   // 작성
+  const { loading } = useSelector((a) => a.loading)
   
   const startLoading = useCallback(() => dispatch(loadingStart()), [dispatch])
   const endLoading = useCallback(() => dispatch(loadingEnd()), [dispatch])
@@ -253,6 +254,12 @@ const ReviewWriteForm = () => {
 
   return (
     <div className="review_write_page">
+      {loading ? (
+        <div className="pullpage_loading">
+          <Spinner animation="border" role="status" />
+        </div>
+      ) : null}
+
       <div>상품 정보</div>
 
       <StarRating
